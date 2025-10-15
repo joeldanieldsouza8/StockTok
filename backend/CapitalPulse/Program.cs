1ddf106260abe3843a3ce4feabb89071bf3c765e
joeld <joeldsouza8@gmail.com>
2025-10-15 13:40:05 +0100

    Added authentication to both the frontend and backend using Auth0.
    Need to add scopes for authorization next.

--- backend/CapitalPulse/Program.cs
+++ backend/CapitalPulse/Program.cs
@@ -1,3 +1,7 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 namespace CapitalPulse;
 
 public class Program
@@ -6,27 +10,53 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+        ConfigureServices(builder);
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
+        ConfigureMiddleware(app);
 
         app.Run();
+    }
+
+    private static void ConfigureServices(WebApplicationBuilder builder)
+    {
+        var services = builder.Services;
+        var configuration = builder.Configuration;
+
+        // Add controllers
+        services.AddControllers();
+
+        var domain = $"https://{configuration["Auth0:Domain"]}/";
+        var audience = configuration["Auth0:Audience"];
+
+        // Configure JWT Bearer authentication
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = domain;
+                options.Audience = audience;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    // ValidAudiences = [audience],
+                    NameClaimType = ClaimTypes.NameIdentifier
+                };
+            });
+    }
+
+    private static void ConfigureMiddleware(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+
+        }
+
+        // app.UseHttpsRedirection(); // TODO: IMPORTANT: Uncomment when in production
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
     }
 }
