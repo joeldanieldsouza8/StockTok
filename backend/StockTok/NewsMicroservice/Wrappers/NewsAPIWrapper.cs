7044813eba9bd0ede2dbcd67d967b516579f4e6d
Zayaan <zayaankashif@yahoo.com>
2025-11-06 01:08:17 +0000

    marketaux api call for news microservice to retrieve through GET is working + next step is to save fields from retrieved news api response into news table inside news db

---
+++ backend/StockTok/NewsMicroservice/Wrappers/NewsAPIWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using NewsMicroservice.Models;
+
+namespace StockTok.NewsMicroservice.Wrappers
+{
+    public class NewsAPIWrapper
+    {
+        [JsonPropertyName("data")]
+        public List<NewsModel> Data { get; set; }
+    }
+}
