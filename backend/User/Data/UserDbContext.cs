bc30cdd4266515ba087dda495c154d5ca389106d
Joel D'Souza <joeldsouza8@gmail.com>
2025-11-06 20:23:54 +0000

    refactor: Overhaul backend project structure with new microservices architecture

---
+++ backend/User/Data/UserDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using User.Data.Configurations;
+
+namespace User.Data;
+
+public class UserDbContext : DbContext
+{
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+    
+    public DbSet<Models.User>  Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        new UserEntityTypeConfiguration().Configure(modelBuilder.Entity<Models.User>());
+    }
+}
