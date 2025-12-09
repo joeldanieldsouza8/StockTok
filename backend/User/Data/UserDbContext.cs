addb32e51c76ec517c39302a570c4c6a21293ec5
sequitria <sequitria@gmail.com>
2025-12-09 02:26:22 +0000

    Added config and model files for watchlists to work, updated User.csproj for migrations
    need to implement CRUD operations

--- backend/User/Data/UserDbContext.cs
+++ backend/User/Data/UserDbContext.cs
@@ -9,6 +9,11 @@ public class UserDbContext : DbContext
     public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
     
     public DbSet<Models.User>  Users { get; set; }
+    
+    // DbSets for watchlist feature
+    public DbSet<Models.Watchlist> Watchlists { get; set; }
+    public DbSet<Models.Ticker> Tickers { get; set; }
+    public DbSet<Models.WatchlistTicker> WatchlistTickers { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
