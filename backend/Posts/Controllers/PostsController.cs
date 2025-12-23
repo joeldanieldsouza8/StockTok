c9329092abf18fd0032f5a3ba71f0c3fa0cf1be7
Joel D'Souza <joeldsouza8@gmail.com>
2025-12-23 19:51:03 +0530

    chore:
    - Adding DTO classes to remove the complete trust of the backend to the frontend.
    - Need to get the claims from the JWT passed in the request.

--- backend/Posts/Controllers/PostsController.cs
+++ backend/Posts/Controllers/PostsController.cs
@@ -1,6 +1,30 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Posts.Models;
+using Posts.Services;
+
 namespace Posts.Controllers;
 
-public class PostsController
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class PostsController : ControllerBase
 {
+    private readonly PostsService _postsService;
+
+    public PostsController(PostsService postsService)
+    {
+        _postsService = postsService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreatePostAsync([FromBody] Post post)
+    {
+        var newPost = await _postsService.CreatePostAsync(post);
+
+        return CreatedAtAction(nameof(GetPostByUserAsync), new { id = newPost.PostId }, new { id = newPost.PostId });
+    }
+    
     
 }
---
+++ backend/Posts/DTOs/CreateCommentDto.cs
@@ -0,0 +1,8 @@
+namespace Posts.DTOs;
+
+public class CreateCommentDto
+{
+    public string Body { get; set; } = string.Empty;
+    
+    
+}
